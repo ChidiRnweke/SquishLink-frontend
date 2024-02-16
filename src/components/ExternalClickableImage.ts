de6710062e3a717559366486e6fa8daed17f1864
ChidiRNweke <chidi125@gmail.com>
2024-02-16 12:36:18 +0100

    Add clickable image component

---
+++ src/components/ExternalClickableImage.ts
@@ -0,0 +1,44 @@
+import { getAttributeOrThrow } from "safe-query";
+
+class ExternalClickableImage extends HTMLElement {
+    public link!: string;
+    public constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.render();
+    }
+
+    private render(): void {
+        this.shadowRoot!.innerHTML = /*html*/`
+<style>
+    :host {
+        cursor: pointer;
+        transition: transform 0.3s ease;
+    }
+
+    :host:hover {
+        transform: scale(1.05);
+
+    }
+
+</style>
+<slot name="image"></slot>
+       `;
+    }
+    public connectedCallback(): void {
+        this.addEventListeners();
+        this.link = getAttributeOrThrow(this, "href")
+
+    }
+
+    private addEventListeners(): void {
+        this.addEventListener('click', (e: MouseEvent) => this.handleLinkClick(e));
+    }
+
+    private handleLinkClick(_event: MouseEvent): void {
+        window.location.href = this.link;
+    }
+
+}
+customElements.define('external-clickable-image', ExternalClickableImage);
+
